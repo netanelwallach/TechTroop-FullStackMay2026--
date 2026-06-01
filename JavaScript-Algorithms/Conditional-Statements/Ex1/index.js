6fb66b2883ae8c5cc8b9b2324f62149f8e77f571
Netanel Wallach <netanelwallach@gmail.com>
2026-06-01 14:58:33 +0300

    JS - Conditional-Statements - Ex1

---
+++ JavaScript-Algorithms/Conditional-Statements/Ex1/index.js
@@ -0,0 +1,8 @@
+let age = 20;
+// Your conditional code here
+
+if (age > 18) {
+  console.log("You're old enough to vote");
+} else {
+  console.log("You're still young - start with your driving license :)");
+}

a7353b67a0543d63d5071762a988ef7e162f295f
Netanel Wallach <netanelwallach@gmail.com>
2026-06-01 15:26:17 +0300

    JS-Conditions-Ex2

---
+++ JavaScript-Algorithms/Conditional-Statements/Ex2/index.js
@@ -0,0 +1,15 @@
+let score = 85;
+// Your conditional code here
+if (score < 60) {
+  console.log("F");
+} else if (score < 70) {
+  console.log("D");
+} else if (score < 80) {
+  console.log("C");
+} else if (score < 90) {
+  console.log("B");
+} else if (score < 100) {
+  console.log("A");
+} else {
+  console.log("Your score is not int 0-100 range");
+}

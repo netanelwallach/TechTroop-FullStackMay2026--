06da292f520eef970c2c837210fa645ecccf4f70
Netanel Wallach <netanelwallach@gmail.com>
2026-06-01 13:54:23 +0300

    Learning JS

--- JavaScript-Algorithms/index.js
+++ JavaScript-Algorithms/index.js
@@ -8,3 +8,15 @@ b = 2;
 
 console.log(a); //prints 6
 console.log(b); //prints 2
+
+const firstString = "car";
+const secondString = "pet";
+const bothStrings = firstString + secondString;
+
+console.log(bothStrings);
+
+console.log(3 < 5); // will output true
+console.log(3 > 5); // will output false
+console.log(2 >= 2); // will output true
+console.log(1 <= -1); // will output  false
+console.log("this string" == "that string"); // will output false

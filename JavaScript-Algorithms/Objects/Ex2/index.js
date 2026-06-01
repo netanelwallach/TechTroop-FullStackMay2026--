2a1720e74ff287b0c9c25debdb147ce823717cc7
Netanel Wallach <netanelwallach@gmail.com>
2026-06-01 17:56:19 +0300

    JS-Objects-Ex2

---
+++ JavaScript-Algorithms/Objects/Ex2/index.js
@@ -0,0 +1,9 @@
+let library = {
+  books: [
+    { title: "A Tale of Love and Darkness", author: "Amos Oz" },
+    { title: "To the End of the Land", author: "David Grossman" },
+    { title: "A Pigeon and a Boy", author: "Meir Shalev" },
+    { title: "Mr. Mani", author: "A.B. Yehoshua" },
+    { title: "The Seven Good Years", author: "Etgar Keret" },
+  ],
+};

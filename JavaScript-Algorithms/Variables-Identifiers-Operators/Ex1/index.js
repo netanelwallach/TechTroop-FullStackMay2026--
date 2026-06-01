d05631d2f87244b497be2cc9ca0675252298399c
Netanel Wallach <netanelwallach@gmail.com>
2026-06-01 14:40:35 +0300

    js begining

---
+++ JavaScript-Algorithms/Variables-Identifiers-Operators/Ex1/index.js
@@ -0,0 +1,19 @@
+// (5 > 2) && false // false because of &&
+// !("knife" === "sword") // true beacuse ! reverses
+// (1 < 2) || (-1 > -1) || !false // true because of ||
+// "" // false
+// (31 % 5) == "1" // true - js knwos to do the convention
+// !!true // true
+// "5th Avenue" != "5th Avenue" // false - same value
+// 52 !== "52" // true
+// (undefined || null) // false
+
+console.log(5 > 2 && false);
+console.log(!("knife" === "sword"));
+console.log(1 < 2 || -1 > -1 || !false);
+console.log(""); // wrong check but it's false
+console.log(31 % 5 == "1");
+console.log(!!true);
+console.log("5th Avenue" != "5th Avenue");
+console.log(52 !== "52");
+console.log(undefined || null); // I got wrong
--- JavaScript-Algorithms/index.js
+++ JavaScript-Algorithms/index.js
@@ -20,3 +20,48 @@ console.log(3 > 5); // will output false
 console.log(2 >= 2); // will output true
 console.log(1 <= -1); // will output  false
 console.log("this string" == "that string"); // will output false
+
+console.log(1 == "1"); // prints true
+console.log(1 === "1"); // prints false
+
+let num;
+console.log(3 + num);
+
+console.log(14 % 5); // will print 4
+
+console.log(23 == "23");
+console.log(41 === 41);
+console.log("this string" == "this string");
+console.log(972 % 2 == 0);
+
+let startPosition;
+let newPosition = (startPosition || 0) + 10;
+
+console.log(newPosition); //prints 10
+//Don't
+function isItMorning(time) {
+  if (time > 6) {
+    if (time < 12) {
+      return true;
+    }
+  }
+  return false;
+}
+// Don't 
+function isItMorning(time){
+    if(time < 6){
+        continue
+    }else{
+        if(time <12){
+            return true
+            }
+    }
+    return false
+}
+// Do!!
+function isItMorning(time){
+    if(time > 6 && time < 12){
+            return true
+    }
+    return false
+}

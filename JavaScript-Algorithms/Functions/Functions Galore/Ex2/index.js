0dc5ffb068b030d2a32518663e1139036b2115a2
Netanel Wallach <netanelwallach@gmail.com>
2026-06-02 15:18:11 +0300

    JS-Functions Galore-Ex2

---
+++ JavaScript-Algorithms/Functions/Functions Galore/Ex2/index.js
@@ -0,0 +1,43 @@
+const story =
+  "In the beginning there was light. Then there were wolves. Finally there was a big fire. Ultimately, Shelob the wolf-master put out the fire with her feet. But until then, the fire caused one heck of a lot of damage.";
+const specialChars = [",", ".", "'", '"', "?", "!", ";"];
+const wordCounts = {};
+
+const cleanText = function (str) {
+  const newStr = str.toLowerCase();
+  return newStr;
+};
+
+const removeSpecialChars = function (str, toRemove) {
+  let result = str;
+  for (const remove of toRemove) {
+    result = result.split(remove).join("");
+  }
+  return result;
+};
+
+const countWords = function (sentence) {
+  let storyLowerCase = cleanText(story);
+  let cleanStory = removeSpecialChars(storyLowerCase, specialChars);
+
+  let words = cleanStory.split(" ");
+
+  for (const word of words) {
+    if (wordCounts[word]) {
+      wordCounts[word] += 1;
+    } else {
+      wordCounts[word] = 1;
+    }
+  }
+
+  return wordCounts;
+};
+
+console.log(countWords(story));
+
+// const storyLowerCase = story.toLowerCase();
+// let cleanStory = storyLowerCase;
+//
+// for (const specialChar of specialChars) {
+//   cleanStory = cleanStory.split(specialChar).join("");
+// }

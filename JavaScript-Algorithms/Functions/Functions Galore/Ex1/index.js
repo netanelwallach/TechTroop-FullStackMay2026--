03e53c0c7cb7c8851a6cfa8b35e17c207c1defa7
Netanel Wallach <netanelwallach@gmail.com>
2026-06-02 07:42:23 +0300

    JS-Functions Galore-Ex1

---
+++ JavaScript-Algorithms/Functions/Functions Galore/Ex1/index.js
@@ -0,0 +1,80 @@
+const people_info = [
+  {
+    name: "guido",
+    profession: "bungalow builder",
+    age: 17,
+    country: "canaland",
+    city: "sydurn",
+    catchphrase: "what a piece of wood!",
+  },
+  {
+    name: "petra",
+    profession: "jet plane mechanic",
+    age: 31,
+    country: "greenmark",
+    city: "bostork",
+    catchphrase: "that's my engine, bub",
+  },
+  {
+    name: "damian",
+    profession: "nursery assistant",
+    age: 72,
+    country: "zimbia",
+    city: "bekyo",
+    catchphrase: "with great responsibility comes great power",
+  },
+];
+
+const capitalize = function (str) {
+  let capitalizedStr = "";
+  capitalizedStr += str[0].toUpperCase(); // first letter, capitalized
+  capitalizedStr += str.slice(1); // rest of the string
+  return capitalizedStr;
+};
+
+const getAge = function (number) {
+  let str = "";
+  if (number < 21) {
+    str = "an Underage";
+  } else if (number <= 55) {
+    str = number + " years old";
+  } else {
+    str = "a 55+";
+  }
+  return str;
+};
+
+const getProfession = function (str) {
+  const words = str.split(" ");
+  let result = "";
+  words.forEach((element) => {
+    result += capitalize(element) + " ";
+  });
+  return result;
+};
+
+const getCityCountry = function (city, country) {
+  let result = capitalize(city) + ", " + capitalize(country);
+  return result;
+};
+
+const getCatchphrase = function (str) {
+  let result = capitalize(str);
+  return result;
+};
+
+const getSummary = function (person) {
+  let summary = "";
+  summary += capitalize(person.name);
+  summary += ` is ${getAge(person.age)} `; //Yes - you can put a function call inside the tick quotes!
+  summary += getProfession(person.profession);
+  summary += `from ${getCityCountry(person.city, person.country)}. `;
+  summary +=
+    capitalize(person.name) +
+    ` loves to say "${getCatchphrase(person.catchphrase)}".`;
+  return summary;
+};
+
+people_info.forEach((element) => {
+  console.log(getSummary(element));
+});
--- JavaScript-Algorithms/Functions/index.js
+++ JavaScript-Algorithms/Functions/index.js
@@ -72,3 +72,51 @@ companies.push("Blizzard Entertainment");
 // employees.push(...candidates);
 // employees.push(candidates[0], candidates[1], candidates[2]);
 companies.splice(1, 1);
+
+const people_info = [
+  {
+    name: "guido",
+    profession: "bungalow builder",
+    age: 17,
+    country: "canaland",
+    city: "sydurn",
+    catchphrase: "what a piece of wood!",
+  },
+  {
+    name: "petra",
+    profession: "jet plane mechanic",
+    age: 31,
+    country: "greenmark",
+    city: "bostork",
+    catchphrase: "that's my engine, bub",
+  },
+  {
+    name: "damian",
+    profession: "nursery assistant",
+    age: 72,
+    country: "zimbia",
+    city: "bekyo",
+    catchphrase: "with great responsibility comes great power",
+  },
+];
+
+const capitalize = function (str) {
+  let capitalizedStr = "";
+  capitalizedStr += str[0].toUpperCase(); // first letter, capitalized
+  capitalizedStr += str.slice(1); // rest of the string
+  return capitalizedStr;
+};
+
+console.log(capitalize("hello")); // "Hello"
+
+const getSummary = function (person) {
+  let summary = "";
+  summary += capitalize(person.name);
+  // summary += ` is ${getAge(person.age)} `; //Yes - you can put a function call inside the tick quotes!
+  // summary += //call function for profession
+  // summary += //call function for country + city
+  // summary += //call function for catchphrase
+  return summary;
+};
+
+console.log(getSummary(people_info[0]));

d0fbec67e59769156b124bb28ab618bf81e77928
Netanel Wallach <netanelwallach@gmail.com>
2026-06-02 17:09:24 +0300

    JS-Arrow-Ex5

---
+++ JavaScript-Algorithms/Callback-Arrow-Function/Ex5/index.js
@@ -0,0 +1,6 @@
+const capitalize = (str) =>
+  console.log(str.charAt(0).toUpperCase() + str.slice(1).toLowerCase());
+
+capitalize("bOb"); // returns Bob
+capitalize("TAYLOR"); // returns Taylor
+capitalize("feliSHIA"); // returns Felishia

d12221bd1671cbc4f2a61df232517d76938c0e2f
Netanel Wallach <netanelwallach@gmail.com>
2026-06-03 09:57:29 +0300

    JS-Recude-Ex-in-class

---
+++ JavaScript-Algorithms/index.js
@@ -0,0 +1,24 @@
+// const sentence =
+//   "This is a sentence that i a repeateble sentence to check sentence occurrences";
+
+const sentence = "the quick brown fox jumps over the lazy dog the fox";
+
+const words = sentence.split(" ");
+
+const occurrences = {};
+for (const word of words) {
+  occurrences[word] = (occurrences[word] || 0) + 1;
+  //   if (occurrences[word]) {
+  //     occurrences[word] += 1;
+  //   } else {
+  //     occurrences[word] = 1;
+  //   }
+}
+console.log(occurrences);
+
+const occurrencesReduced = words.reduce((counts, word) => {
+  counts[word] = (counts[word] || 0) + 1;
+  return counts;
+}, {});
+
+console.log(occurrencesReduced);

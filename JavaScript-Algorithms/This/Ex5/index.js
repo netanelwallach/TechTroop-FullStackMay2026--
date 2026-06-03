dc307efd705e14a94a92727fe18b9d2caf21c925
Netanel Wallach <netanelwallach@gmail.com>
2026-06-03 11:03:39 +0300

    JS-This-Ex5

---
+++ JavaScript-Algorithms/This/Ex5/index.js
@@ -0,0 +1,26 @@
+const revealSecret = function () {
+  return this.secret;
+};
+
+const shoutIt = function (person, func) {
+  person.revealItAll = func;
+  const result = person.revealItAll();
+  console.log(person.name + " said: " + result);
+};
+
+const avi = {
+  name: "Avi",
+  secret: "Im scared of snakes!",
+};
+
+const narkis = {
+  name: "Narkis",
+  secret: "I don't have secrets because I'm zen like that.",
+};
+
+shoutIt(avi, revealSecret);
+shoutIt(narkis, revealSecret);
+
+// For my understanding
+// console.log(avi.revealItAll());
+// console.log(narkis.revealItAll());

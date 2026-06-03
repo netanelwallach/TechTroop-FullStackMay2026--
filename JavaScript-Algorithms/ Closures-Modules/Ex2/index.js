d6954dcb9f6ef0709853453f91f24e259de21afd
Netanel Wallach <netanelwallach@gmail.com>
2026-06-03 20:55:33 +0300

    JS-Closures & Modules-Eס2

---
+++ JavaScript-Algorithms/ Closures-Modules/Ex2/index.js
@@ -0,0 +1,20 @@
+const Bank = function () {
+  let money = 500;
+  const depositCash = function (num) {
+    money += num;
+  };
+
+  const checkBalance = function () {
+    console.log(money);
+  };
+
+  return {
+    deposit: depositCash,
+    showBalance: checkBalance,
+  };
+};
+
+const bank = Bank();
+bank.deposit(200);
+bank.deposit(250);
+bank.showBalance(); //should print 950

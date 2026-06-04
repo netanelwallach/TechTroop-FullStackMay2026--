0e7fae059e4a81de1e09c21acc8726c07c46b994
Netanel Wallach <netanelwallach@gmail.com>
2026-06-04 17:33:17 +0300

    Big-O -Time-Complexity-Ex1

---
+++ JavaScript-Algorithms/Big-O-Time-Complexity/Ex1/index.js
@@ -0,0 +1,11 @@
+const getBalance = function (bankOperations) {
+  let balance = 0;
+  for (let op of bankOperations) {
+    balance += op;
+  }
+  return balance;
+};
+
+getBalance([-27, -43, -2400, -700, 15000, 58]);
+
+// Complexity - O(n)

388e1d64e621753081aa0ec774fd5815b365ff6e
Netanel Wallach <netanelwallach@gmail.com>
2026-06-08 11:49:47 +0300

    JS-Console App & Input-registration

--- JavaScript-Algorithms/Console-App-Input/Ex3/register.mjs
+++ JavaScript-Algorithms/Console-App-Input/Ex3/register.mjs
@@ -5,19 +5,18 @@ const rl = readline.createInterface({ input, output });
 
 console.log("--- User Registration System --- \n");
 
-// Sequential questions
-const name = await rl.question("Enter your name: ");
-const email = await rl.question("Enter your email: ");
-const age = await rl.question("Enter your age: ");
-const favoriteColor = await rl.question("Enter your favorite color: ");
+rl.question("Enter your name: ", (name) => {
+  rl.question("Enter your email: ", (email) => {
+    rl.question("Enter your age: ", (age) => {
+      rl.question("Enter your favorite color: ", (favoriteColor) => {
+        rl.close();
 
-rl.close(); // Closes the interface immediately after gathering input
-
-// Summary output
-console.log("\nRegistration Summary:");
-console.log(`Name: ${name}`);
-console.log(`Email: ${email}`);
-console.log(`Age: ${age}`);
-console.log(`Favorite Color: ${favoriteColor}`);
-
-// no Error handling the code
+        console.log("\nRegistration Summary:");
+        console.log(`Name: ${name}`);
+        console.log(`Email: ${email}`);
+        console.log(`Age: ${age}`);
+        console.log(`Favorite Color: ${favoriteColor}`);
+      });
+    });
+  });
+});

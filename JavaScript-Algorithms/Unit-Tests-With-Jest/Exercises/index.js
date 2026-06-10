2e2f149bd8a8322c2bac5be270de00723ddb71fa
Netanel Wallach <netanelwallach@gmail.com>
2026-06-10 11:11:43 +0300

    JS - Unit Tests With Jest - Ex2

--- JavaScript-Algorithms/Unit-Tests-With-Jest/Exercises/index.js
+++ JavaScript-Algorithms/Unit-Tests-With-Jest/Exercises/index.js
@@ -3,6 +3,14 @@ const isEven = function (n) {
   return n % 2 == 0 ? true : false;
 };
 
+//should remove at least one element from the array `arr`
+const removeAtLeastOne = function (arr) {
+  let numItemsToRemove = Math.floor(Math.random() * (arr.length - 1)) + 1;
+  arr.splice(0, numItemsToRemove);
+  return arr;
+};
+
 module.exports = {
   isEven,
+  removeAtLeastOne,
 };
--- JavaScript-Algorithms/Unit-Tests-With-Jest/Exercises/index.test.js
+++ JavaScript-Algorithms/Unit-Tests-With-Jest/Exercises/index.test.js
@@ -1,5 +1,13 @@
-const { isEven } = require(".");
+const { isEven, removeAtLeastOne } = require(".");
 
 test("isEven should return true for 58", () => {
   expect(isEven(58)).toBeTruthy();
+});
+
+test("removeAtLeastOne should return an array that is smaller at least in one from the origianl array", () => {
+  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const size = arr.length;
+  let arr2 = removeAtLeastOne(arr);
+  const size2 = arr2.length;
+  expect(size2).toBeLessThan(size);
 });

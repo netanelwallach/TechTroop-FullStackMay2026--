47918307aaf7dda383d60ca079e0d1cc576fbdd1
Netanel Wallach <netanelwallach@gmail.com>
2026-06-15 11:35:49 +0300

    oldAsyncFunctionPromise

--- JavaScript-WebDev/index.js
+++ JavaScript-WebDev/index.js
@@ -17,3 +17,32 @@ function rollDice() {
 rollDice()
   .then((result) => console.log(`You rolled a ${result}`))
   .catch((error) => console.error(`Roll failed: ${error.message}`));
+
+function oldAsyncFunction(callback) {
+  setTimeout(() => {
+    if (Math.random() > 0.5) {
+      callback(null, "Success");
+    } else {
+      callback(new Error("Failed"));
+    }
+  }, 1000);
+}
+
+function oldAsyncFunctionPromise() {
+  return new Promise((resolve, reject) => {
+    oldAsyncFunction((err, result) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(result);
+    });
+  });
+}
+
+oldAsyncFunctionPromise()
+  .then((result) => {
+    console.log("Result:", result);
+  })
+  .catch((error) => {
+    console.error("Error caught:", error.message);
+  });

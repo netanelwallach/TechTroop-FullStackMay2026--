29cddc5581e4d9db40d426f6eaee3af270a432ca
Netanel Wallach <netanelwallach@gmail.com>
2026-06-16 17:42:55 +0300

    Js-DataFlow-Ex

--- JavaScript-WebDev/Data Flow/Ex1/script.js
+++ JavaScript-WebDev/Data Flow/Ex1/script.js
@@ -1,11 +1,17 @@
 import { getPosts, addPost } from "./model.js";
 
-const container = document.getElementsByClassName("form-container")[0];
+// const container = document.getElementsByClassName("form-container")[0];
+const postsList = document.getElementById("posts-list");
+
 function render(arr) {
+  if (postsList) {
+    postsList.innerHTML = "";
+  }
   for (let i = 0; i < arr.length; i++) {
     const div = document.createElement("div");
     div.innerText = `Post: ${arr[i].name} - ${arr[i].text}`;
-    container.appendChild(div);
+    // container.appendChild(div);
+    postsList.appendChild(div);
   }
 }
 

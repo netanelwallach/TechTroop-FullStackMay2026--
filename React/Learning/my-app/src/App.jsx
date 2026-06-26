ca279f8232d4c1fa090c0cf570a59da593c56773
Netanel Wallach <netanelwallach@gmail.com>
2026-06-26 19:26:34 +0300

    React Intro & JSX - Ex2

--- React/Learning/my-app/src/App.jsx
+++ React/Learning/my-app/src/App.jsx
@@ -1,9 +1,11 @@
 import CompanyInfo from "./components/Ex1/CompanyInfo";
+import WeatherComp from "./components/Ex2/Weather";
 
 function App() {
   return (
     <>
       <CompanyInfo />
+      <WeatherComp />
     </>
   );
 }
---
+++ React/Learning/my-app/src/components/Ex2/Weather.jsx
@@ -0,0 +1,33 @@
+import "./Weather.css";
+
+function WeatherComp() {
+  const getClassName = (temperature) => {
+    let className = "";
+    switch (true) {
+      case temperature < 15:
+        className = "freezing";
+        break;
+      case temperature >= 15 && temperature <= 30:
+        className = "fair";
+        break;
+      case temperature > 30:
+        className = "hell-scape";
+        break;
+      default:
+        break;
+    }
+
+    return className;
+  };
+
+  return (
+    <div className="ex-space">
+      <h4 className="ex-title">Exercise 2</h4>
+      <div className="exercise" id="ex-2">
+        <div id="weatherBox" className={getClassName(14)}></div>
+      </div>
+    </div>
+  );
+}
+
+export default WeatherComp;

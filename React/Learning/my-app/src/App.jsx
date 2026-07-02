86a4426833b95019374b50fe9aa63003c8eeabe6
Netanel Wallach <netanelwallach@gmail.com>
2026-07-02 13:14:48 +0300

    React Input - Ex1

--- React/Learning/my-app/src/App.jsx
+++ React/Learning/my-app/src/App.jsx
@@ -2,14 +2,18 @@ import CompanyInfo from "./components/Ex1/CompanyInfo";
 import WeatherComp from "./components/Ex2/Weather";
 import Hudini from "./state/Ex1/Hudini";
 import StoreApp from "./state/Ex2/StoreApp";
+import Exercise1 from "./input/Ex1/Exercise1";
 
 function App() {
   return (
     <>
-      <CompanyInfo />
+      {
+        /* <CompanyInfo />
       <WeatherComp />
       <Hudini />
-      <StoreApp />
+      <StoreApp /> */
+        <Exercise1 />
+      }
     </>
   );
 }
